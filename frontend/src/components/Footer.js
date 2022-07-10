b52d1189e4fb2aa0192e0407a01e561d6abd1948
Nassim Boulghib <nassim.boulghib@epitech.eu>
2022-07-10 11:38:43 +0200

    fix footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -2,9 +2,9 @@ import React from "react";
 
 const Footer = () => {
     return (
-        <div className="flex justify-center items-center w-full h-20 px-4 text-sm text-white bg-gradient-to-b from-gray-800 to-black">
+        <footer className="flex justify-center items-center w-full h-20 px-4 text-sm text-white bg-gradient-to-b from-gray-800 via-gray-800 to-black">
             <p>© Copyright Merce 2022</p>
-        </div>
+        </footer>
     );
 };
 
